294fa413b504375d34b2c580e3accb216da7d032
TH4KUR <max910payne@gmail.com>
2021-09-28 19:49:48 +0530

    Some changes...

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 'use-strict';
 
+const applyClassList = () => {
+  let classs = Array.from(document.querySelector('ol').children);
+  console.log(classs);
+  // Adding Classes list Styles
+  classs.forEach((el) => {
+    el.style.background = '#222';
+  });
+};
+
 const applyDark = () => {
-  console.log('laoded');
   let currentUrl = document.location.href;
   if (currentUrl.includes('classroom.google.com')) {
     // Selecting the required elements
     let body = document.querySelector('body');
     let nav = document.querySelector('nav');
-    let classes =
-      Array.from(document.querySelector('ol').children) ||
-      setTimeout(() => Array.from(document.querySelector('ol').children), 3000);
     // Apply Styles
+    setTimeout(() => {}, 3000);
     body.style.backgroundColor = '#111111f5';
     nav.style.background = '#222';
     nav.style.borderBottom = '0.0625rem solid #666';
